2f4958402a869fcf605f2e160abbbb911b734806
Ralph Bean <rbean@redhat.com>
2025-01-27 13:28:45 -0500

    Optimize webhook extraction logic to reduce duplicate key lookups

--- sync2jira/compat.py
+++ sync2jira/compat.py
@@ -4,11 +4,9 @@ def extract_message_body(msg):
     Handle both fedmsg and fedora-messaging style message bodies.
     """
 
-    if "body" in msg:
-        return msg["body"]
-    elif "msg" in msg:
-        return msg["msg"]
-    else:
-        raise KeyError(
-            f"Unrecognized message format with keys {msg.keys()}. Expected either 'msg' or 'body'"
-        )
+    body = msg.get("body", msg.get("msg"))
+    if body:
+        return body
+    raise KeyError(
+        f"Unrecognized message format with keys {msg.keys()}. Expected either 'msg' or 'body'"
+    )
