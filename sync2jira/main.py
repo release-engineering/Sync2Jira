dc04b1596cedced3108e27b5ce54a985175ba65f
Ralph Bean <rbean@redhat.com>
2025-03-19 13:29:56 -0400

    Allow multiple consumers from the messaging queue
    This will hopefully resolve our hanging process problem and let
    crashloops proceed more fluidly.

--- sync2jira/main.py
+++ sync2jira/main.py
@@ -177,7 +177,7 @@ def listen(config):
         queue: {
             "durable": True,  # Persist the queue on broker restart
             "auto_delete": False,  # Delete the queue when the client terminates
-            "exclusive": True,  # Disallow multiple simultaneous consumers
+            "exclusive": False,  # Allow multiple simultaneous consumers
             "arguments": {},
         },
     }
