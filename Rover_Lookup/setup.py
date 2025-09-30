630a7f0b9c2828b5f5fbe05465406c0981404fd1
Webb Scales <wscales@redhat.com>
2025-09-30 13:21:01 -0400

    Review feedback:  correct the Rover Lookup license classifier

--- Rover_Lookup/setup.py
+++ Rover_Lookup/setup.py
@@ -26,7 +26,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
